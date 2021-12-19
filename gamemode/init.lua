2eee5d2370d044f86c4977cc333e70b20c7e5603
no4ka <konstantin@kochura.ru>
2021-12-19 20:04:54 +0300

    GM:Initialize()

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,3 +1,7 @@
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 include( "shared.lua" )
+
+function GM:Initialize()
+    print('I`m OK! :)')
+end
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -2,6 +2,3 @@ GM.Name = "Murder"
 GM.Author = "N/A"
 GM.Email = "N/A"
 GM.Website = "N/A"
-
-function GM:Initialize()
-end
